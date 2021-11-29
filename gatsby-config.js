10d6e481c521d072dbc2397d18a9baff026528ac
Bryce Beckenbach <brycebeckenbach@gmail.com>
2021-11-29 13:57:41 -0500

    updated page title and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Bryce Beckenbach | Software Engineer`,
+        short_name: `Bryce Beckenbach`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
