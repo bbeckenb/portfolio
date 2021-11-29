0b947cc078cbd5dc3ccfb35f65d8614b2e076ad0
Bryce Beckenbach <brycebeckenbach@gmail.com>
2021-11-29 18:17:44 -0500

    updated page title and favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,19 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <meta property="og:image" content="/src/images/PortfolioPic.png" />
-        <meta property="image" content="/src/images/PortfolioPic.png" />
+        <meta property="og:image" content="./PortfolioPic.png" data-react-helmet="true" />
+        <meta property="image" content="./PortfolioPic.png" data-react-helmet="true" />
+        <meta
+          property="og:title"
+          content="Bryce Beckenbach | Software Engineer"
+          data-react-helmet="true"
+        />
         <title>Bryce Beckenbach | Software Engineer</title>
+        <meta
+          property="og:description"
+          content="Portfolio site to display my most up-to-date technical skills, projects, and contact information"
+          data-rh="true"
+        />
         <html lang={lang || 'en'} />
         <meta name="description" content={description || 'Gatsby Simplefolio'} />
       </Helmet>
